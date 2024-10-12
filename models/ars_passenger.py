ee9ce31e3e22a6bce4ea4bef640717550bd90109
Joel Kiggwe <joelkiggwe94@gmail.com>
2024-10-12 13:50:38 +0300

    Compute the display name in the passeger view

--- models/ars_passenger.py
+++ models/ars_passenger.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import base64
+# import base64
 
 from odoo import models, fields, api
-from odoo.tools import file_open
+# from odoo.tools import file_open
 
 
 class Passenger(models.Model):
     _name = 'ars.passenger'
     _description = 'ARS Passenger Information'
-    _rec_name = 'ref'
+    _rec_name = 'display_name'
     _order = 'create_date DESC'
 
     # @api.model
@@ -18,7 +18,8 @@ class Passenger(models.Model):
 
     ref = fields.Char("Passenger ID")
     passenger_photo = fields.Image('Passenger Photo', max_width=128, max_height=128, compute_sudo=True)
-    fname = fields.Char("First Name")
+    name = fields.Char(string='Passenger Name', default='/', copy=False, compute='_compute_display_name')
+    fname = fields.Char("First Name", required=True)
     middlename = fields.Char("Middle Name")
     lname = fields.Char("Last Name")
     email = fields.Char("Email")
@@ -38,6 +39,17 @@ class Passenger(models.Model):
         for passenger in res:
             passenger['ref'] = 'ARS/PASSGR/' + str(res.id).zfill(4)
         return res
+
+    def _compute_display_name(self):
+        for record in self:
+            _name = ''
+            if record.fname and record.lname:
+                _name = record.fname if record.fname else _name
+                _name = _name + ' ' + record.middlename if record.middlename else _name
+                _name = _name + ' ' + record.lname if record.lname else _name
+                record.display_name = _name
+            else:
+                record.display_name = record.fname
 
     def save_contact(self):
         return True
