c6c47f48803c12c053a05a1b5d2f9e4c2783a75a
Joel Kiggwe <joelkiggwe94@gmail.com>
2024-10-13 00:06:49 +0300

    Add ticket number generation on creation

--- models/ars_reservation.py
+++ models/ars_reservation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api,_
 
 
 class AirportReservation(models.Model):
@@ -9,8 +9,7 @@ class AirportReservation(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _order = 'create_date DESC'
 
-    name = fields.Char(string="Booking Ref", help="Reservation Number")
-    book_date = fields.Date(string="Date of Booking")
+    name = fields.Char(string="Booking Ref", help="Reservation Number", default='/', readonly=1)
     book_datetime = fields.Datetime(string="Booking Date")
     total_amount = fields.Float(string="Total Amount")
 
@@ -24,6 +23,19 @@ class AirportReservation(models.Model):
     passenger_id = fields.Many2one('ars.passenger', string='Booked by')
 
     status = fields.Selection([('draft', 'Draft'), ('save', 'Set')], default='draft', string="Booking Status")
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('name', _('/')) == _('/'):
+                vals['name'] = self.env['ir.sequence'].next_by_code(
+                    'ars.reservation')
+
+                # Set the default booking date if not provided
+            if not vals.get('book_datetime'):
+                vals['book_datetime'] = fields.Datetime.now()
+
+        return super().create(vals_list)
 
     # ---------------------------------------------------------
     # Business Methods
--- models/ars_ticket.py
+++ models/ars_ticket.py
@@ -7,7 +7,7 @@ class AircraftTicket(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _rec_name = "ticket_no"
 
-    ticket_no = fields.Char(string="Ticket No.")
+    ticket_no = fields.Char(string="Ticket No.", default='/', readonly=True, copy=False)
     book_ref = fields.Many2one("ars.reservation", string="Booking ID")
     passenger_id = fields.Many2one('ars.passenger',string="Passenger Name")
     contact_data = fields.Char(string="Contact Information")
@@ -17,3 +17,22 @@ class AircraftTicket(models.Model):
     flight_available_seats = fields.Integer(string='Available Seats',
                                             related='flight_ticket_id.available_seats')
     ticket_price = fields.Float(string='Price',default=0.0)
+
+    @api.onchange('book_ref')
+    def _onchange_flight_number(self):
+        for rec in self:
+            if rec.book_ref:
+                rec.flight_ticket_id = rec.book_ref.flight_bk_id
+
+    @api.model
+    def create(self, vals_list):
+        ticket = super(AircraftTicket, self).create(vals_list)
+
+        for rec, val in zip(ticket, vals_list):
+            # flight_ref = rec.flight_ticket_id.flight_no
+            flight_ref = self.env['ars.flight'].browse(rec.flight_ticket_id.id).flight_no
+            if flight_ref:
+                rec['ticket_no'] = f'{flight_ref}/TK000{ticket.id}'
+            else:
+                rec['ticket_no'] = f'TK000{ticket.id}'
+        return ticket
