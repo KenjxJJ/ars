7c7cb03302ba051098b3a1b80d64aa36f3699165
Joel Kiggwe <joelkiggwe94@gmail.com>
2024-10-13 21:33:17 +0300

    Add compute on the contact info in ticket generation

--- models/ars_ticket.py
+++ models/ars_ticket.py
@@ -12,7 +12,7 @@ class AircraftTicket(models.Model):
     ticket_no = fields.Char(string="Ticket No.", default='/', readonly=True, copy=False)
     book_ref = fields.Many2one("ars.reservation", string="Booking ID")
     passenger_id = fields.Many2one('ars.passenger',string="Passenger Name")
-    contact_data = fields.Char(string="Contact Information")
+    contact_data = fields.Text(string="Contact Information", compute='_compute_contact_info')
 
     # Flight Information
     flight_ticket_id = fields.Many2one('ars.flight', string="Flight Number")
@@ -57,4 +57,18 @@ class AircraftTicket(models.Model):
         for rec in self:
             fl_price = rec.flight_ticket_id.cost_price if rec.flight_ticket_id else 0.0
             rec.ticket_price = fl_price
+
+    @api.depends('passenger_id')
+    def _compute_contact_info(self):
+        for rec in self:
+            rec.contact_data = ''
+
+            if rec.passenger_id:
+                street = rec.passenger_id.street if rec.passenger_id.street else ""
+                email = rec.passenger_id.email if rec.passenger_id.email else ""
+                mobile = rec.passenger_id.mobile if rec.passenger_id.mobile else ""
+                city = rec.passenger_id.city if rec.passenger_id.city else ""
+                rec.contact_data = f'Tel: {mobile}\nEmail: {email}\nStreet: {street}\nCity: {city}'
+            else:
+                rec.contact_data = ''
 
